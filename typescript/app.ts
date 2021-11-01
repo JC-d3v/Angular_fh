df98ad5df28e8ecb9e9137a41c1cdebb69f56344
JC-d3v <calderon.rjorge@gmail.com>
2021-11-01 18:06:49 -0400

    ts01

---
+++ typescript/app.ts
@@ -0,0 +1,10 @@
+(function () {
+  function saludar(nombre: string) {
+    console.table('Hola ' + nombre); // Hola Logan
+  }
+
+  const wolverine = {
+    nombre: 'Logan',
+  };
+  saludar(wolverine.nombre);
+})();
