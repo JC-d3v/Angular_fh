4206765bd851aa3b49bcce1c3e9fa4a946dc053a
Jorge Calderon <calderon.rjorge@gmail.com>
2022-11-16 22:29:35 -0400

    07-13 mostrando resultados en pantalla

--- 21_02-gifsApp/src/app/gifs/services/gifs.service.ts
+++ 21_02-gifsApp/src/app/gifs/services/gifs.service.ts
@@ -9,6 +9,8 @@ export class GifsService {
   private _apiKey: string = 'mWUk3cu7HhdiZ8R5KUPNpbRCJiBDTgIY';
   private _historial: string[] = [];
 
+  public resultados: any[] = [];
+
   get historial() {
     return [...this._historial];
 
@@ -24,9 +26,10 @@ export class GifsService {
       this._historial = this._historial.splice(0, 10);
     }
 
-    this._http.get('https://api.giphy.com/v1/gifs/search?api_key=mWUk3cu7HhdiZ8R5KUPNpbRCJiBDTgIY&q=terminator&limit=10')
+    this._http.get(`https://api.giphy.com/v1/gifs/search?api_key=mWUk3cu7HhdiZ8R5KUPNpbRCJiBDTgIY&q=${query}&limit=10`)
       .subscribe((resp: any) => {
-        console.log(resp.data)
+        console.log(resp.data);
+        this.resultados = resp.data;
       })
 
   }
