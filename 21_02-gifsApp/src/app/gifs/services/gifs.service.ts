6c3236ba7ff56ad5b1e1b1125a83376c9b1c1c37
Jorge Calderon <calderon.rjorge@gmail.com>
2022-11-20 16:25:58 -0400

    07-16 cargar amagenes automticamente

--- 21_02-gifsApp/src/app/gifs/services/gifs.service.ts
+++ 21_02-gifsApp/src/app/gifs/services/gifs.service.ts
@@ -16,7 +16,12 @@ export class GifsService {
     return [...this._historial];
 
   }
-  constructor(private _http: HttpClient) { }
+  constructor(private _http: HttpClient) {
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+    this.resultados = JSON.parse(localStorage.getItem('resultados')!) || [];
+  }
+
+
 
   buscarGifs(query: string) {
 
@@ -25,12 +30,16 @@ export class GifsService {
     if (!this._historial.includes(query)) {
       this._historial.unshift(query);
       this._historial = this._historial.splice(0, 10);
+
+      localStorage.setItem('historial', JSON.stringify(this._historial));
+
     }
 
     this._http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=mWUk3cu7HhdiZ8R5KUPNpbRCJiBDTgIY&q=${query}&limit=10`)
       .subscribe((resp) => {
         console.log(resp.data);
         this.resultados = resp.data;
+        localStorage.setItem('resultados', JSON.stringify(this.resultados));
       })
 
   }
